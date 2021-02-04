2066d859382ff4ac8e7c802e1c3a92722c3611bc
Steven Zhang <YZhang@lcs.on.ca>
2021-02-04 09:47:59 -0500

    Initial Commit

---
+++ FavouriteThing/FavouriteThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingApp.swift
+//  FavouriteThing
+//
+//  Created by Steven Zhang on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
